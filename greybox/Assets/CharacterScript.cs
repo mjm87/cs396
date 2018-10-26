c1feb535488997e227234dea40f18d5bf826ee1b
Matthew Getz <mdg7@calvin.edu>
2018-10-26 15:45:51 -0400

    first cube named Steven

---
+++ greybox/Assets/CharacterScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CharacterScript : MonoBehaviour {
+
+    public string name;
+
+	// Use this for initialization
+	void Start () {
+        //GameObject cubeLabel = GameObject.Find("CubeName");
+        //cubeLabel.GetComponent<TextMeshProUGUI>().text = name;
+
+        gameObject.GetComponentInChildren<TextMeshPro>().text = name;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ greybox/Assets/PlayerScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Translate(Vector3.forward*Time.deltaTime*Input.GetAxis("Vertical")*speed);
+        transform.Translate(Vector3.right * Time.deltaTime * Input.GetAxis("Horizontal")*speed);
+
+     
+    }
+}
